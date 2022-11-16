503c4ce57535c956f82c5116b15d7f7d5cf29228
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2022-11-16 07:33:22 +0100

    missing file

---
+++ HueAPIV2.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE EmptyDataDeriving #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module HueAPIV2 where
+
+
+import Prelude ()
+import Prelude.Compat
+import MyAeson
+import Data.Aeson
+import GHC.Generics
+import Servant
+import Data.Time.Clock
+
+import MQTTAPI (ColorXY(..))
+
+type HueApiV2 = "eventstream" :> "clip" :> "v2" :> StreamGet NewlineFraming JSON (SourceIO Event)
+
+data ResourceType = LightRes | SensorRes deriving (Eq,Show,Generic)
+instance ToJSON ResourceType where
+  toJSON = \case
+    LightRes -> "light"
+    SensorRes -> "sensor"
+
+data Brightness = Brightness { brightness :: Int } deriving (Eq,Show,Generic)
+data Color = Color { xy :: ColorXY }deriving (Eq,Show,Generic)
+data Temperature = Temperature { mirek :: Int }deriving (Eq,Show,Generic)
+
+data Event = Event {
+  resource :: ResourceType,
+  idv1 :: String,
+  idv2 :: String,
+  creationTime :: UTCTime,
+  dimming :: Brightness,
+  color :: Maybe Color,
+  color_temperature :: Maybe Temperature
+} deriving (Eq,Show,Generic)
+
+
+
+instance ToJSON Brightness
+instance ToJSON Color
+instance ToJSON Temperature
+instance ToJSON Event
+
