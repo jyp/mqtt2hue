39effde281726960f4f1ec22f83b2d75932b9cb6
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2022-11-20 08:32:30 +0100

    oops

--- Server.hs
+++ Server.hs
@@ -187,7 +187,7 @@ groupAction :: String -> Int -> HueAPI.Action -> HueHandler Text.Text
 groupAction _userId groupId a0 = do
   liftIO $ Text.putStrLn ("[[[ " <> Text.pack (show groupId) <> " " <> Text.pack (show a0))
   (t,a) <- withAppState (handleGroupAction groupId a0)
-  let Just t' = mkTopic (t <> "/set") 
+  let Just t' = mkTopic t
   appPublish t' a
   return "Updated."
 
